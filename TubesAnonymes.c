db9ac741dfbce94af29b1b7dbfcd7259ed2789c4
Thomas-Thiboutot <90419892+Thomas-Thiboutot@users.noreply.github.com>
2023-03-09 12:50:01 -0500

    Update TubesAnonymes.c

--- TubesAnonymes.c
+++ TubesAnonymes.c
@@ -1,4 +1,3 @@
-// ajouter les directives d'inclusion nécessaires
 #include <unistd.h>
 #include <stdio.h>
 #include <sys/wait.h>
@@ -8,10 +7,7 @@ int main() {
 
     int cmpResult;
 
-    char *cat_args[] = {"cat", "scores", NULL};
-    char *grep_args[] = {"grep", "Villanova", NULL};
-    char *cut_args[] = {"cut", "-b", "1-10", NULL};
-
+   
     int pipes[4];
     pipe(pipes); // sets up 1st pipe
     pipe(pipes + 2); // sets up 2nd pipe
@@ -21,9 +17,6 @@ int main() {
         if( fork() == 0) {
 
             if( fork() == 0) {
-                
-                int fp = open("In.txt", O_WRONLY | O_RDONLY);
-                dup2(fp, 0);
                 dup2(pipes[1], 1);
 
                 close(pipes[0]);
@@ -41,15 +34,16 @@ int main() {
             close(pipes[2]);
             close(pipes[3]);
 
-            execl("usr/bin/rev", "rev", "In.txt", NULL);
+            execl("usr/bin/rev", "rev", NULL);
         }
         
         dup2(pipes[2], 0);
+
         close(pipes[0]);
         close(pipes[1]);
         close(pipes[2]);
         close(pipes[3]);
-        execl("usr/bin/cmp", "cmp", "-","In.txt", NULL);
+        execl("usr/bin/cmp", "cmp", "-", "In.txt", "-s" ,NULL);
     }
     close(pipes[0]);
     close(pipes[1]);
